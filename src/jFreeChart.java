56a6ebe8d1a1de87732ec5a15b69748827c2de7c
Taras Iks <ikstaras@gmail.com>
2018-01-12 02:43:08 +0100

    first commit

---
+++ src/jFreeChart.java
@@ -0,0 +1,51 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
+public class jFreeChart extends ApplicationFrame {
+
+	/**
+	 * ChartPanel.getChart().getXYPlot().getDomainAxis().setInverted(boolean) 
+	 */
+	private static final long serialVersionUID = -2519667984268422493L;
+
+	public jFreeChart(String applicationTitle, String chartTitle) {
+		super(applicationTitle);
+		JFreeChart lineChart = ChartFactory.createLineChart(chartTitle,
+				"Years", "Number of Schools", createDataset(),
+				PlotOrientation.VERTICAL, true, true, false);
+
+		lineChart.getXYPlot().getDomainAxis().setInverted(true);
+		
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		//chartPanel.getChart().getXYPlot().getRangeAxis().setInverted(true);
+		chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+		setContentPane(chartPanel);
+	}
+
+	private DefaultCategoryDataset createDataset() {
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(15, "schools", "1970");
+		dataset.addValue(30, "schools", "1980");
+		dataset.addValue(60, "schools", "1990");
+		dataset.addValue(120, "schools", "2000");
+		dataset.addValue(240, "schools", "2010");
+		dataset.addValue(300, "schools", "2014");
+		return dataset;
+	}
+
+	public static void main(String[] args) {
+		jFreeChart chart = new jFreeChart("School Vs Years",
+				"Numer of Schools vs years");
+
+		chart.pack();
+		
+		RefineryUtilities.centerFrameOnScreen(chart);
+		chart.setVisible(true);
+	}
+}
